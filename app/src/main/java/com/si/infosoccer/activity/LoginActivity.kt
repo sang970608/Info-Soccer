8720556112e52cd23bbcb02c91c94fafee712ddf
Lucy <lucyy9901@gmail.com>
2023-02-28 01:36:29 +0900

    구글 로그인

--- app/src/main/java/com/si/infosoccer/activity/LoginActivity.kt
+++ app/src/main/java/com/si/infosoccer/activity/LoginActivity.kt
@@ -1,17 +1,44 @@
 package com.si.infosoccer.activity
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.si.infosoccer.R
+
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var mGoogleSignInClient : GoogleSignInClient? = null
+    var googleLoginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == -1) {
+            val data = result.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            getGoogleInfo(task)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
         googleLL.setOnClickListener{
             // Google 로그인
+            googleLogin()
         }
         naverLL.setOnClickListener{
             // Naver 로그인
@@ -22,5 +49,23 @@ class LoginActivity : AppCompatActivity() {
         kakaoLL.setOnClickListener {
             // 이외 로그인
         }
+    }
+
+    private fun getGoogleInfo(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val TAG = "구글 로그인 결과"
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.d(TAG, account.id!!)
+            Log.d(TAG, account.familyName!!)
+            Log.d(TAG, account.givenName!!)
+            Log.d(TAG, account.email!!)
+        }
+        catch (e: ApiException) {
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+    private fun googleLogin() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        googleLoginLauncher.launch(signInIntent)
     }
 }
